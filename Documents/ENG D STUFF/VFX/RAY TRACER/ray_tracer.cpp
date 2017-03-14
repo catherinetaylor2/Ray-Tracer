f0aa153b2a4dbec5d3549dd79ac04ecd6508f196
Catherine <cat.taylor.taylor@btinternet.com>
2017-03-14 16:22:24 +0000

    Added Gouraud shading

--- Documents/ENG D STUFF/VFX/RAY TRACER/ray_tracer.cpp
+++ Documents/ENG D STUFF/VFX/RAY TRACER/ray_tracer.cpp
@@ -102,7 +102,7 @@ int main(int argc, char* argv[] ){
 			tri.set_lighting_constants(0.7, 0.8*255, 0.3, 400);
 
 			if (t!=0){
-				float ss;
+				float ss,  alpha1, alpha2, alpha3, area, A,B,C, semiPerimeter, semiPerimeter1, semiPerimeter2,semiPerimeter3, P_P1, P_P2, P_P3, R,G, Bc;
 				int s = 1;
 
 				vector3 point = d.vec_add(eye, d.vec_scal_mult(t-0.00001f,d));
@@ -111,21 +111,55 @@ int main(int argc, char* argv[] ){
 				for (int k=0; k<F; k++){
 					c1 = FV[3*k] -1, c2 = FV[3*k+1]-1, c3 = FV[3*k+2] -1 ;
 					triangle tri2(V[3*c1], V[3*c1+1], V[3*c1+2], V[3*c2], V[3*c2+1], V[3*c2+2], V[3*c3], V[3*c3+1], V[3*c3+2], RED);
-					ss = tri2.ray_triangle_intersection( point, l);
-					
-					if ((ss)> 0){;
+
+					ss = tri2.ray_triangle_intersection( point, l);			
+					if ((ss)> 0){
 						s = 0;						
 					}			
 				 } 
 
 				vector3 normal=tri.get_triangle_normal(tri.get_vertex1(), tri.get_vertex2(), tri.get_vertex3());  
-				vector3 RGB = tri.determine_colour(point, l, d, sun, normal, myscene,s);
-				
-				img[x] = RGB.get_x();
-				img[x+1]=RGB.get_y();
-				img[x+2]=RGB.get_z(); 
+				c1 = FV[3*min_value] -1, c2 = FV[3*min_value+1]-1, c3 = FV[3*min_value+2] -1 ;
+				vector3 point1(V[3*c1], V[3*c1+1], V[3*c1+2]);
+				vector3 point2(V[3*c2], V[3*c2+1], V[3*c2+2]);
+				vector3 point3(V[3*c3], V[3*c3+1], V[3*c3+2]);
+				A = sqrt(point1.dotproduct(point1.vec_add(point1, point1.vec_scal_mult(-1, point2)),point1.vec_add(point1, point1.vec_scal_mult(-1, point2))));
+				B = sqrt(point1.dotproduct(point1.vec_add(point3, point1.vec_scal_mult(-1, point2)),point1.vec_add(point3, point1.vec_scal_mult(-1, point2))));
+				C = sqrt(point1.dotproduct(point1.vec_add(point1, point1.vec_scal_mult(-1, point3)),point1.vec_add(point1, point1.vec_scal_mult(-1, point3))));
+				P_P1 = sqrt(point1.dotproduct(point1.vec_add(point1, point1.vec_scal_mult(-1, point)),point1.vec_add(point1, point1.vec_scal_mult(-1, point))));
+				P_P2= sqrt(point1.dotproduct(point1.vec_add(point, point1.vec_scal_mult(-1, point2)),point1.vec_add(point, point1.vec_scal_mult(-1, point2))));
+				P_P3 = sqrt(point1.dotproduct(point1.vec_add(point, point1.vec_scal_mult(-1, point3)),point1.vec_add(point, point1.vec_scal_mult(-1, point3))));
+
+				semiPerimeter = (A+B+C)/2.0f;
+				semiPerimeter1 = (P_P3+B+P_P2)/2.0f;
+				semiPerimeter2 = (P_P3+C+P_P1)/2.0f;
+				semiPerimeter3 = (A+P_P2+P_P1)/2.0f;
+				area = sqrt(semiPerimeter*(semiPerimeter - A)*(semiPerimeter- B)*(semiPerimeter -C));
+				alpha1 = sqrt(semiPerimeter1*(semiPerimeter1 - B)*(semiPerimeter1- P_P2)*(semiPerimeter1 -P_P3))/area;
+				alpha2 = sqrt(semiPerimeter2*(semiPerimeter2 - C)*(semiPerimeter2- P_P1)*(semiPerimeter2 -P_P3))/area;
+				alpha3 = sqrt(semiPerimeter3*(semiPerimeter3 - A)*(semiPerimeter3- P_P2)*(semiPerimeter3 -P_P1))/area;
+	
+				vector3 RGB1 = tri.determine_colour(point1, l, d, sun, normal, myscene,s);
+				vector3 RGB2 = tri.determine_colour(point2, l, d, sun, normal, myscene,s);
+				vector3 RGB3 = tri.determine_colour(point3, l, d, sun, normal, myscene,s);
+				R = RGB1.get_x()*alpha1+RGB2.get_x()*alpha2+RGB3.get_x()*alpha3;
+				G = RGB1.get_y()*alpha1+RGB2.get_y()*alpha2+RGB3.get_y()*alpha3;
+				Bc = RGB1.get_z()*alpha1+RGB2.get_z()*alpha2+RGB3.get_z()*alpha3;
+				if (R > 255){
+					R=255;
+				}
+				if (G > 255){
+					G=255;
+				}
+				if (Bc > 255){
+					Bc=255;
+				}
+				img[x] = R;
+				img[x+1]=G;
+				img[x+2]=Bc;
 			}
 		}
+		delete t_values;
 	}
 
     ofstream my_image;
@@ -143,6 +177,7 @@ int main(int argc, char* argv[] ){
     delete FN;
     delete V;
     delete N;
+
 
 
     return 0;
